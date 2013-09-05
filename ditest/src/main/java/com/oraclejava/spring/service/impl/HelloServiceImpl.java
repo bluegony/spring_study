c10c1493b69506ae43276c87f4d6722f48f9a0c1
bluegony <bluegonu@gmail.com>
2013-09-05 17:41:26 +0900

    aa

--- ditest/src/main/java/com/oraclejava/spring/service/impl/HelloServiceImpl.java
+++ ditest/src/main/java/com/oraclejava/spring/service/impl/HelloServiceImpl.java
@@ -7,13 +7,20 @@ import org.springframework.stereotype.Component;
 import com.oraclejava.spring.service.HelloService;
 import com.oraclejava.spring.service.Sms;
 
-@Component
+
 public class HelloServiceImpl implements HelloService {
 
-	@Autowired
-	@Qualifier("sktelecom")
+
 	private Sms sms;
 	
+	public Sms getSms() {
+		return sms;
+	}
+
+	public void setSms(Sms sms) {
+		this.sms = sms;
+	}
+
 	public void hello(){
         System.out.println( "Hello World!" );
         sms.send("aaa");
